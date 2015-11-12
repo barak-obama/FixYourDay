069c5dea2c84cbc9bc78826475ec49641e2cac2c
obama <baral.oh16@gmail.com>
2015-11-12 18:18:48 +0200

    rbvewblivbwelvhe

--- app/src/main/java/spikes/fixyourday/NewScheduleActivity.java
+++ app/src/main/java/spikes/fixyourday/NewScheduleActivity.java
@@ -1,7 +1,11 @@
 package spikes.fixyourday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NewScheduleActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class NewScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule);
+        Button r = (Button) findViewById(R.id.new_schedule_button);
+        r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(this, DisplayMessageActivity.class);
+                Toast.makeText(null, "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
