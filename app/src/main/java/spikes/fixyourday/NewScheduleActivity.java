082e5f3dc69836c2f372cc012681c83878f7a4a3
dvir42 <dviros7@gmail.com>
2015-11-12 21:12:14 +0200

    bla

--- app/src/main/java/spikes/fixyourday/NewScheduleActivity.java
+++ app/src/main/java/spikes/fixyourday/NewScheduleActivity.java
@@ -3,13 +3,10 @@ package spikes.fixyourday;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.widget.Button;
-=======
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
->>>>>>> 069c5dea2c84cbc9bc78826475ec49641e2cac2c
 
 public class NewScheduleActivity extends AppCompatActivity {
 
@@ -17,10 +14,6 @@ public class NewScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule);
-<<<<<<< HEAD
-        Button b = (Button)findViewById(R.id.button);
-        b.setText("hi");
-=======
         Button r = (Button) findViewById(R.id.new_schedule_button);
         r.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +22,6 @@ public class NewScheduleActivity extends AppCompatActivity {
                 Toast.makeText(null, "Hello", Toast.LENGTH_LONG).show();
             }
         });
->>>>>>> 069c5dea2c84cbc9bc78826475ec49641e2cac2c
     }
 
 }
